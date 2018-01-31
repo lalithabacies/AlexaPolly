cd542f93519e0da6e7052eb176310eb3ea365294
lalithabacies <lalith@abacies>
2018-01-31 17:56:14 +0530

    deploy all features

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 """Getting Started Example for Python 2.7+/3.3+"""
 from boto3 import Session
-from botocore.exceptions import BotoCoreError, ClientError
+#from botocore.exceptions import BotoCoreError, ClientError
 from contextlib import closing
 import os
 import sys
