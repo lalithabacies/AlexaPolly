3037df0d17a3c5313dba568bf6558bf3baa50308
lalithabacies <lalith@abacies>
2018-02-02 16:07:44 +0530

    add changes of schedule

--- __init__.py
+++ __init__.py
@@ -14,6 +14,8 @@ import datetime
 from apscheduler.schedulers.background import BackgroundScheduler
 import atexit
 import time
+import threading
+from threading import Thread
 
 app=Flask(__name__)
 app.secret_key = "amazon_polly_0343sdsad@#$#$%vb2u2"
@@ -22,6 +24,12 @@ aws_access_key_id = "AKIAIXQSPFFFCBPHBAHQ"
 aws_secret_access_key = "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"
 region_name="us-west-2" 
 
+s3 = boto3.client('s3',aws_access_key_id=aws_access_key_id,aws_secret_access_key=aws_secret_access_key)
+# Create a client using the credentials and region defined in the [adminuser]
+# section of the AWS credentials file (~/.aws/credentials).
+session = Session(aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key,region_name=region_name)
+polly = session.client("polly")
+    
 @app.route('/',methods=['GET','POST'])
 def homepage():      
     return ''
@@ -65,13 +73,6 @@ def get_text(option=''):
 @app.route('/get_shortaudio',methods=['GET','POST'])
 def get_shortaudio(combined_url='',option='',limit='1'):
     now = datetime.datetime.now()
-    date_time = now.strftime("%Y-%m-%d_%H-%M-%S")
-    
-    s3 = boto3.client('s3',aws_access_key_id=aws_access_key_id,aws_secret_access_key=aws_secret_access_key)
-    # Create a client using the credentials and region defined in the [adminuser]
-    # section of the AWS credentials file (~/.aws/credentials).
-    session = Session(aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key,region_name=region_name)
-    polly = session.client("polly") 
     
     try:
         if 'option' in request.args:
@@ -80,54 +81,112 @@ def get_shortaudio(combined_url='',option='',limit='1'):
         option=''
         
     split_string = lambda x, n: [x[i:i+n] for i in range(0, len(x), n)]
-    splitted_text = split_string(str(get_text(option)),1000)
     sounds       = []
     
     if limit=='1':
         length=1
+        splitted_text= split_string(str(get_text(option)),1000)
     else:
-        length=len(splitted_text)
-    
-    for l in range(0,length):
-        # Request speech synthesis
-        response = polly.synthesize_speech(Text=str(splitted_text[l]), OutputFormat="mp3", VoiceId="Joey")
+        splitted_text= split_string(str(get_text(option)),1400)
+        length=len(splitted_text)   
+    # l=0
+    # response = polly.synthesize_speech(Text=str(splitted_text[l]), OutputFormat="mp3", VoiceId="Joey")
         
-        if "AudioStream" in response:
-            # Note: Closing the stream is important as the service throttles on the
-            # number of parallel connections. Here we are using contextlib.closing to
-            # ensure the close method of the stream object will be called automatically
-            # at the end of the with statement's scope.
-            with closing(response["AudioStream"]) as stream:
-                output = "speech"+str(l)+"_"+str(date_time)+".mp3"
+    # if "AudioStream" in response:
+        # # Note: Closing the stream is important as the service throttles on the
+        # # number of parallel connections. Here we are using contextlib.closing to
+        # # ensure the close method of the stream object will be called automatically
+        # # at the end of the with statement's scope.
+        # with closing(response["AudioStream"]) as stream:
+            # output = "speech"+str(l)+"_"+str(date_time)+".mp3"
 
-                try:
-                    # Open a file for writing the output as a binary stream
-                    with open(output, "wb") as file:
-                        file.write(stream.read())
-                    bucket_name = 'amazon-polly'
-                    filename    = output
-                    s3.upload_file(filename, bucket_name, filename,
-                        ExtraArgs={'ACL': 'public-read'}
-                    )
-                    s3_url = 'https://s3-us-west-2.amazonaws.com/%s/%s' % (bucket_name, filename)
-                    sounds.append(s3_url)
-                except IOError as error:
-                    # Could not write to file, exit gracefully
-                    print(error)
-                    #sys.exit(-1)                       
-        else:
-            # The response didn't contain audio data, exit gracefully
-            print("Could not stream audio")
-            #sys.exit(-1)
+            # try:
+                # # Open a file for writing the output as a binary stream
+                # with open(output, "wb") as file:
+                    # file.write(stream.read())
+                # bucket_name = 'amazon-polly'
+                # filename    = output
+                # s3.upload_file(filename, bucket_name, filename,
+                    # ExtraArgs={'ACL': 'public-read'}
+                # )
+                # s3_url = 'https://s3-us-west-2.amazonaws.com/%s/%s' % (bucket_name, filename)
+                # sounds.append(s3_url)
+            # except IOError as error:
+                # # Could not write to file, exit gracefully
+                # print(error)
+                # #sys.exit(-1)                       
+    # else:
+        # # The response didn't contain audio data, exit gracefully
+        # print("Could not stream audio")
+        # #sys.exit(-1)
+        
+    for l in range(0,length):
+        date_time = now.strftime("%Y-%m-%d_%H-%M-%S")
+        output = "speech"+str(l)+"_"+str(date_time)+".mp3"
+        sounds.append(output)
+        Thread(target = make_URLs_ready(length,sounds,splitted_text,l)).start()   
+
+    short_s3urls=[]
+    for url in sounds:
+        short_s3urls.append(str("https://s3-us-west-2.amazonaws.com/amazon-polly/"+str(url)))
             
     if limit=='full':            
-        r = requests.post('https://amazon-polly-mergemp3.herokuapp.com/', json = {'Files_To_Merge':sounds,"combined_url":combined_url},headers={'Content-type': 'application/json'}) 
+        #r = requests.post('https://amazon-polly-mergemp3.herokuapp.com/', json = {'Files_To_Merge':sounds,"combined_url":combined_url},headers={'Content-type': 'application/json'}) 
+        
+        # scheduler = BackgroundScheduler()
+        # scheduler.start()    
+        # scheduler.add_job(
+            # func=lambda: make_URLs_ready(length,Short_ulrs,splitted_text),
+            # id='longaudio',
+            # name="contains splitted mp3's",
+            # replace_existing=True)
+        # atexit.register(lambda: scheduler.shutdown())
         #time.sleep(5)
-              
-    return json.dumps(sounds)   
-    #return json.dumps(["https://s3-us-west-2.amazonaws.com/amazon-polly/speech0_2018-02-01_14-37-17.mp3", "https://s3-us-west-2.amazonaws.com/amazon-polly/speech1_2018-02-01_14-37-17.mp3", "https://s3-us-west-2.amazonaws.com/amazon-polly/speech2_2018-02-01_14-37-17.mp3"])    
+        
+        
+        r = requests.post('http://localhost/AmazonPolly_mp3merge_api/index.php', json = {'Files_To_Merge':short_s3urls,"combined_url":combined_url},headers={'Content-type': 'application/json'})
+        return str("https://s3-us-west-2.amazonaws.com/amazon-polly/"+str(combined_url))
+    else:  
+        return json.dumps(short_s3urls)
+        #return json.dumps(sounds)   
+        #return json.dumps(["https://s3-us-west-2.amazonaws.com/amazon-polly/speech0_2018-02-01_14-37-17.mp3", "https://s3-us-west-2.amazonaws.com/amazon-polly/speech1_2018-02-01_14-37-17.mp3", "https://s3-us-west-2.amazonaws.com/amazon-polly/speech2_2018-02-01_14-37-17.mp3"])    
 
     
+def make_URLs_ready(length,urls,splitted_text,l):
+    #for l in range(1,length):
+    # Request speech synthesis
+    response = polly.synthesize_speech(Text=str(splitted_text[l]), OutputFormat="mp3", VoiceId="Joey")
+    
+    if "AudioStream" in response:
+        # Note: Closing the stream is important as the service throttles on the
+        # number of parallel connections. Here we are using contextlib.closing to
+        # ensure the close method of the stream object will be called automatically
+        # at the end of the with statement's scope.
+        with closing(response["AudioStream"]) as stream:
+            #output = "speech"+str(l)+"_"+str(date_time)+".mp3"
+            output = urls[l]
+
+            try:
+                # Open a file for writing the output as a binary stream
+                with open(output, "wb") as file:
+                    file.write(stream.read())
+                bucket_name = 'amazon-polly'
+                filename    = output
+                s3.upload_file(filename, bucket_name, filename,
+                    ExtraArgs={'ACL': 'public-read'}
+                )
+                s3_url = 'https://s3-us-west-2.amazonaws.com/%s/%s' % (bucket_name, filename)
+                #sounds.append(s3_url)
+            except IOError as error:
+                # Could not write to file, exit gracefully
+                print(error)
+                #sys.exit(-1)                       
+    else:
+        # The response didn't contain audio data, exit gracefully
+        print("Could not stream audio")
+        #sys.exit(-1)
+    return ''            
+        
 @app.route('/get_longaudio',methods=['GET','POST'])
 def get_longaudio(option=''):
     now = datetime.datetime.now()
@@ -136,24 +195,24 @@ def get_longaudio(option=''):
     if 'option' in request.args:
         option = request.args.get('option')
 
-    scheduler = BackgroundScheduler()
-    scheduler.start()    
-    scheduler.add_job(
-        func=lambda: get_shortaudio(combined_url,option,'full'),
-        id='longsudio',
-        name='merging mp3 files',
-        replace_existing=True)
-    atexit.register(lambda: scheduler.shutdown())
+    # scheduler = BackgroundScheduler()
+    # scheduler.start()    
+    # scheduler.add_job(
+        # func=lambda: get_shortaudio(combined_url,option,'full'),
+        # id='longsudio',
+        # name='merging mp3 files',
+        # replace_existing=True)
+    # atexit.register(lambda: scheduler.shutdown())
     
-    #get_shortaudio(combined_url,option,'full')
+    urls = get_shortaudio(combined_url,option,'full')
     #url = json.loads(get_shortaudio(combined_url,option,1))
     
     #short_audio_urls = get_shortaudio(option,'full')
     #r = requests.post('http://localhost/AmazonPolly_mp3merge_api/index.php', json = {'Files_To_Merge':json.loads(short_audio_urls),"combined_url":combined_url},headers={'Content-type': 'application/json'})
     
     #return str(r.json()['combined_mp3'])
-    return str("https://s3-us-west-2.amazonaws.com/amazon-polly/"+str(combined_url))
-    #return ''
+    #return str("https://s3-us-west-2.amazonaws.com/amazon-polly/"+str(combined_url))
+    return json.dumps(urls)
 
                 
 
